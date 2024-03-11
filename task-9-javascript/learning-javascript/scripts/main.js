d15ee5b60be00443ab5724629a83ba13179e23ba
jatinraju1704 <jatin.raju@appfoster.com>
2024-03-11 12:57:16 +0530

    learning js

--- task-9-javascript/learning-javascript/scripts/main.js
+++ task-9-javascript/learning-javascript/scripts/main.js
@@ -1,5 +1,6 @@
 // const myHeading = document.querySelector("h1");
 // myHeading.textContent = "Hello World!";
+// -----------------------------------------------------------------------------------------------------
 
 // let myNum = 20;
 // if(myNum % 2 == 0)
@@ -9,6 +10,7 @@
 // else {
 //     alert("Odd Number");
 // }
+// -----------------------------------------------------------------------------------------------------
 
 
 // function sumOfNumbers(n1, n2) {
@@ -29,26 +31,41 @@
 // });
 
 // -----------------------------------------------------------------------------------------------------
-const myImg = document.getElementById("image-container");
-// const myImg = document.querySelector("img");
-// console.log(`image Attribute : ${myImg}`);
-const myHead = document.getElementById("heading-container");
+// const myImg = document.getElementById("image-container");
+// // const myImg = document.querySelector("img");
+// // console.log(`image Attribute : ${myImg}`);
+// const myHead = document.getElementById("heading-container");
 
-myImg.onclick = () => {
-    const mySrc = myImg.getAttribute("src");
-    // console.log(`image Attribute : ${mySrc}`);
-    //  console.log(`head Attribute : ${myHead}`);
-    // console.log(`style Attribute : ${myStyle.getAttribute("background-color")}`);
+// myImg.onclick = () => {
+//     const mySrc = myImg.getAttribute("src");
+//     // console.log(`image Attribute : ${mySrc}`);
+//     //  console.log(`head Attribute : ${myHead}`);
+//     // console.log(`style Attribute : ${myStyle.getAttribute("background-color")}`);
     
-    if(mySrc === "/task-9-javascript/learning-javascript/images/fundamentals.svg") {
-        myImg.setAttribute("src", "/task-9-javascript/learning-javascript/images/react.svg");
-        myHead.textContent = "Image React";
-        myHead.style.color = "red";
-    } 
-    else {
-        myImg.setAttribute("src", "/task-9-javascript/learning-javascript/images/fundamentals.svg");
-        myHead.textContent = "Image Fundamentals";
-        myHead.style.color = "blue";
-    }
-};
+//     if(mySrc === "/task-9-javascript/learning-javascript/images/fundamentals.svg") {
+//         myImg.setAttribute("src", "/task-9-javascript/learning-javascript/images/react.svg");
+//         myHead.textContent = "Image React";
+//         myHead.style.color = "red";
+//     } 
+//     else {
+//         myImg.setAttribute("src", "/task-9-javascript/learning-javascript/images/fundamentals.svg");
+//         myHead.textContent = "Image Fundamentals";
+//         myHead.style.color = "blue";
+//     }
+// };
+// -----------------------------------------------------------------------------------------------------
+
+// let myButton = document.querySelector("button");
+// let myHeading = document.getElementById("name-heading");
+
+// function setUserName() {
+//     const myName = prompt("Please enter your name: ");
+//     localStorage.setItem("name", myName);
+//     myHeading.textContent = localStorage.getItem("name");
+// }
+
+// myButton.onclick = ()=>{
+//     setUserName();
+// };
+
 // -----------------------------------------------------------------------------------------------------
