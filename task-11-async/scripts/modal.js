e27477cd4b8d42e1182a1ef9ef2a17fb874a3aa7
jatinraju1704 <jatin.raju@appfoster.com>
2024-03-13 11:23:59 +0530

    minor changes done

--- task-11-async/scripts/modal.js
+++
@@ -1,9 +0,0 @@
-
-function getUserDetails(element) {
-    document.getElementById('modalTitle').textContent = `ID : ${element.id}`;
-
-    document.getElementById('u_name').textContent = `Name : ${element.name}`;
-    document.getElementById('u_gender').textContent = `Gender : ${element.gender}`;
-    document.getElementById('u_email').textContent = `Email : ${element.email}`;
-    document.getElementById('u_status').textContent = `Status : ${element.status}`;
-}
