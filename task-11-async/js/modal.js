49e1515dd1d5e64191ee61e3e7e65ae78fe1fbf0
jatinraju1704 <jatin.raju@appfoster.com>
2024-03-27 15:24:44 +0530

    added second api call

--- task-11-async/js/modal.js
+++ task-11-async/js/modal.js
@@ -1,11 +1,21 @@
+async function getUserData(id) {
+    let response = await fetch(`https://gorest.co.in/public/v2/users/${id}`);
+    let data = await response.json();
+    return data;
+}
 
-//* create api for user to fetch data 
-// Here
-function getUserDetails(element) {
-    document.getElementById('modalTitle').textContent = `ID : ${element.id}`;
-
-    document.getElementById('u_name').textContent = `Name : ${element.name}`;
-    document.getElementById('u_gender').textContent = `Gender : ${element.gender}`;
-    document.getElementById('u_email').textContent = `Email : ${element.email}`;
-    document.getElementById('u_status').textContent = `Status : ${element.status}`;
+function getUserDetails(id) {
+    getUserData(id).then(
+        data => {
+            console.log(data['id']);
+            document.getElementById('modalTitle').textContent = `ID : ${data['id']}`;
+            document.getElementById('u_name').textContent = `Name : ${data['name']}`;
+            document.getElementById('u_gender').textContent = `Gender : ${data['gender']}`;
+            document.getElementById('u_email').textContent = `Email : ${data['email']}`;
+            document.getElementById('u_status').textContent = `Status : ${data['status']}`;
+        }
+    ).catch(
+        error => {
+            document.getElementById('msg').innerHTML = `Error : ${error}`;
+        });
 }
