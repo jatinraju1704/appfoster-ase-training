c55de5c9c843fbf247d4a62a5a93e733b4037824
jatinraju1704 <jatin.raju@appfoster.com>
2024-03-27 15:34:27 +0530

    task 13 backend api laravel

---
+++ task-13-laravel/user-project-app/routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\UserProjectController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+//* User's API
+Route::post("/host/user/create", [UserController::class, 'create']);
+Route::get("/host/user/get", [UserController::class, 'getAll']);
+Route::get("/host/user/get/{id}", [UserController::class, 'getByid']);
+Route::delete("/host/user/delete/{id}", [UserController::class, 'deleteById']);
+Route::put("/host/user/update/{id}", [UserController::class, 'UpdateById']);
+
+
+//* Project's API
+Route::post("/host/project/create", [ProjectController::class, 'create']);
+Route::get("/host/project/get", [ProjectController::class, 'getAll']);
+Route::get("/host/project/get/{pid}", [ProjectController::class, 'getById']);
+Route::delete("/host/project/delete/{pid}", [ProjectController::class, 'deleteById']);
+Route::put("/host/project/update/{pid}", [ProjectController::class, 'UpdateById']);
+
+//* User-Project API
+Route::post("/host/user/project",[UserProjectController::class, 'addUserProject']);
+Route::get("/host/user/{id}/project",[UserProjectController::class, 'getProjectsByUserId']);
